a8bdeb7bc3cb1b6a48f15129afee15e409166e84
Xiaohu <yelihu.ylh@alibaba-inc.com>
2025-05-14 14:58:24 +0800

    feat(MybatisPlusConfig): 添加分页插件和防全表更新插件
    在 `MybatisPlusConfig` 中添加了 `PaginationInnerInterceptor` 和 `BlockAttackInnerInterceptor` 插件，增强了数据查询的安全性和性能。同时，在 `pom.xml` 文件中管理了 `mybatis-plus-bom` 依赖并增加了 `mybatis-plus-jsqlparser` 依赖。

--- app1/app1-service/src/main/java/com/example/app1/config/MybatisPlusConfig.java
+++ app1/app1-service/src/main/java/com/example/app1/config/MybatisPlusConfig.java
@@ -2,11 +2,13 @@ package com.example.app1.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
 /**
  * MyBatis-Plus配置类
@@ -22,16 +24,15 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        
-        // 分页插件
-        //interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        
+
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
         // 乐观锁插件
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
-        
-         //防止全表更新与删除插件
-        //interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
-        
+
+        //防止全表更新与删除插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+
         return interceptor;
     }
 } 
